03bc0131cdda11b7d23e839054a41823715517fb
luisa <luisa.melo5645955@edu.pe.senac.br>
2026-08-19 21:08:24 -0300

    escrevi o codigo

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("JavaScript conectado!");
+
+//const titulo = document.querySelector("h1");
+//console.log(titulo);
+
+//const hero = document.querySelector("#hero");
+//console.log(hero.textContent);
+
+//const linkSocial = document.querySelector(".link-social");
+//console.log(link-social);
