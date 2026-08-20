f259564593c6199fa48890bc13599957c5b56676
luisa <luisa.melo5645955@edu.pe.senac.br>
2026-08-20 14:50:38 -0300

    adicionei "Desenvolvedora Front-end" no titulo

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 console.log("JavaScript conectado!");
+console.log("Document");
 
 const titulo = document.querySelector("h1");
 console.log(titulo);
 
-//const hero = document.querySelector("#hero");
-//console.log(hero.textContent);
+const hero = document.querySelector("#hero");
+console.log(hero.textContent);
 
 //const linkSocial = document.querySelector(".link-social");
 //console.log(link-social);
